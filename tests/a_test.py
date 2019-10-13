a753a8718a472e70740481730926b43d5a79f7fb
George Necula <necula@google.com>
2019-10-13 18:34:54 +0200

    Wrote the loops version

--- tests/a_test.py
+++ tests/a_test.py
@@ -64,6 +64,28 @@ class Render(object):
 
 
   def render1(self, presence, types, templates, background):
+    """
+    Render template instances into a batch of images.
+
+    The dimensions are: batch of B images, of size (H,W) with C channels. There are T templates,
+    each described by an image (TH,TW) with C channels.
+
+    Assumptions: At each pixel there is at most one template instance center.
+
+    :param presence: a tensor[B, H, W], for each batch and each pixel, an entry +inf if at that
+      pixel we have the center of some template instantion. And -inf if there is no template
+      whose center is at the pixel. TODO: I think this can be derived from "types".
+    :param types: a tensor[B, H, W, T], for each batch and each pixel and each template type,
+      we have +inf if there is an instance of that template type at that pixel. We put -inf
+      for all the other templates at that pixel.
+    :param templates: a tensor[T, TH, TW, C], for each template type, its image.
+    :param background: a tensor[B, H, W, C] with the background
+    :return: a tensor[B, H, W, C], for each batch and pixel, the average of the contributions
+      of each template instance that overlap the pixel. For each template instance we
+      use the corresponding pixel from the template. If no template instance overlaps the
+      we use the background for the pixel.
+    """
+
     # Extract and verify input shapes. pylint: disable=invalid-name
     # In the comments, I use TH->h, TW->w ("small h") for clarity.
     inf = 1e5  # Such that inf*0 = 0 and not nan, but fits into float32 with +1
@@ -130,37 +152,98 @@ class Render(object):
     # return a bit more than just the image, for debug purposes.
     return img, (a, a_bg, t)
 
+  def render_loops(self, presence, types, templates, background):
+    """Render template instances into a batch of images.
+
+    See doc string for render1 above.
+    """
+    # Extract and verify input shapes. pylint: disable=invalid-name
+    # In the comments, I use TH->h, TW->w ("small h") for clarity.
+    inf = 1e5  # Such that inf*0 = 0 and not nan, but fits into float32 with +1
+
+    B, H, W = presence.shape
+    B2, H2, W2, T = types.shape
+    assert B == B2 and H == H2 and W == W2, "Bad types shape"
+    T, TH, TW, C = templates.shape
+    assert TH % 2 == 1 and TW % 2 == 1  # For now the simpler case of odd-size dimensions,
+                                        # for which the center is clear
+    B3, H3, W3, C2 = background.shape
+    assert B3 == B2 and H3 == H2 and W3 == W2 and C2 == C, "Bad background shape"
+
+    # The output: BHW
+    output = np.zeros((B, H, W, C))
+
+    # For each batch
+    for b in range(B):
+      # For each pixel in the image
+      for i in range(H):
+        for j in range(W):
+          # Compute how many templates contribute to this pixel. At the same time
+          # add up their contributions.
+          count = 0.
+          sum = np.zeros(C)  # The total contribution to each channel
+          # For each template type
+          for t in range(T):
+            # A template contributes to this pixel if its center is not too far. Iterate
+            # over the possible centers that can contribute to this pixel
+            for ci in range(i - TH//2, i + TH//2 + 1):
+              for cj in range(j - TW//2, j + TW//2 + 1):
+                # If this center is still in the image
+                if (ci >= 0 and ci < H) and (cj >= 0 and cj < W):
+                  if types[b, ci, cj, t] > 0.0:
+                    # An instance of this template is present here
+                    count += 1.
+                    # Compute the indices into the template
+                    ti = i - (ci - TH//2)
+                    tj = j - (cj - TW//2)
+                    # Due to the mirroring in the input templates
+                    ti = TH - 1 - ti
+                    tj = TW - 1 - tj
+                    print("At [{},{}] template {}[{},{}] contributes {}".format(
+                      i, j, t, ti, tj, templates[t, ti, tj, 0]
+                    ))
+                    for c in range(C):
+                      sum[c] += templates[t, ti, tj, c]
+          # Now divide the sum to get the average
+          if count > 0.:
+            for c in range(C):
+              output[b, i, j, c] = sum[c] / count
+          else:
+            # It's a background pixel
+            for c in range(C):
+              output[b, i, j, c] = background[b, i, j, c]
+
+    return output
+
 
 class RenderTest(jtu.JaxTestCase):
 
   def test_render1(self):
     # 1. Create the test data.
+    # See the docstring of the render1 function.
+    #
     inf = 1e5  # Such that inf*0 = 0 and not nan, but fits into float32 with +1
                # and we get 1/0 when softmaxing with logits using this.
 
     H, W = 1, 5  # pylint: disable=invalid-name
     # Template types:
     T = 2  # pylint: disable=invalid-name
-    # We assume an image 1x5 with two template instances aligned as follows:
-    #  Pixel: 0  1  2  3  4
-    #  T1:    -  T1 T1 T1 -
-    #  T2:    -  -   - T2 T2
+
+    # We assume an image 1x5 with two template instances (one instance of T1
+    # and one instance of T2), aligned as follows:
+    #  Pixel: 0  1   2   3   4
+    #  0:     -  T1 T1 T1+T2 T2
     #
     # Presence logits: HW
-    # For each pixel, we use 'inf' if the image contains an instance
-    # of some template centered at that pixel. The 'types' tensor below
-    # says which template it is.
-    # We use -inf for a pixel if it does not contain the center of
-    # a template instance.
     presence = np.array([-inf, -inf, inf, -inf, inf], np.float32).reshape(H, W)
 
     # Type logits: HWT
-    # For each pixel, and each template, whether the pixel contains the center
-    # of that template (+inf). And -inf ???
-    types = np.array([[0., 0., +inf, 0., -inf],
-                      [0., 0., -inf, 0., +inf]], np.float32).T.reshape(H, W, T)
+    types = np.array([[0., 0., +inf, 0., -inf],  # Template 0
+                      [0., 0., -inf, 0., +inf]], # Template 1
+                     np.float32).T.reshape(H, W, T)
 
     # Templates: ThwC
+    # We have two templates, with shape 1x3 and 1 channel.
     TH, TW, C = 1, 3, 1  # pylint: disable=invalid-name
     # NOTE: there is currently a mirroring happening (conv vs filt). If there
     # was no mirroring, the first one is what we'd want to use for this test,
@@ -172,19 +255,82 @@ class RenderTest(jtu.JaxTestCase):
     templates = np.array([[1.0, 0.5, 0.3],
                           [10.0, 5.0, 3.0]], np.float32).reshape(T, TH, TW, C)
 
+
     # Background: HWC
     background = np.full((H, W, C), 9.9, np.float32)
 
     # Desired painting: HWC
     desired = np.array([9.9,
-                        1.0*0.3 + 0.0*3.0,
-                        1.0*0.5 + 0.0*5.0,
-                        0.5*1.0 + 0.5*3.0,
-                        0.0*1.0 + 1.0*5.0], np.float32).reshape(H, W, C)
+                        1.0*0.3 + 0.0*3.0,  # T1_0
+                        1.0*0.5 + 0.0*5.0,  # T1_1
+                        0.5*1.0 + 0.5*3.0,  # T1_2 + T2_0
+                        0.0*1.0 + 1.0*5.0], # T2_1
+                       np.float32).reshape(H, W, C)
 
     render = Render()
     # Add the batch-dimension where necessary
-    actual, extra = render.render1(
-        presence[None], types[None], templates, background[None])
-    print(actual, extra)
-    np.testing.assert_allclose(actual[0], desired)
+    img, (a, a_bg, t) = render.render1(
+        presence[np.newaxis], types[np.newaxis], templates, background[np.newaxis])
+    print("\nimg=", img, "\na=", a, "\na_bg=", a_bg, "\nt=", t)
+    np.testing.assert_allclose(img[0], desired)
+
+    img_loops = render.render_loops(presence[np.newaxis], types[np.newaxis], templates, background[np.newaxis])
+    print("\nimg=", img)
+    np.testing.assert_allclose(img_loops[0], desired)
+
+  def test_render2(self):
+    # 1. Create the test data.
+    inf = 1e5  # Such that inf*0 = 0 and not nan, but fits into float32 with +1
+               # and we get 1/0 when softmaxing with logits using this.
+
+    H, W = 1, 5  # pylint: disable=invalid-name
+    # Template types:
+    T = 2  # pylint: disable=invalid-name
+
+    # We assume an image 1x5 with three template instances (two instances of T1
+    # and one instance of T2), aligned as follows:
+    #  Pixel: 0  1    2       3      4
+    #  0:     -  T1 T1+T1 T1+T1+T2 T1+T2
+    #
+    # Presence logits: HW
+    presence = np.array([-inf, -inf, inf, inf, inf], np.float32).reshape(H, W)
+
+    # Type logits: HWT
+    types = np.array([[0., 0., +inf, +inf, -inf],  # Template 0 (2 instances)
+                      [0., 0., -inf, -inf, +inf]], # Template 1 (1 instance)
+                     np.float32).T.reshape(H, W, T)
+
+    # Templates: ThwC
+    # We have two templates, with shape 1x3 and 1 channel.
+    TH, TW, C = 1, 3, 1  # pylint: disable=invalid-name
+    # NOTE: there is currently a mirroring happening (conv vs filt). If there
+    # was no mirroring, the first one is what we'd want to use for this test,
+    # but in order to account for the mirroring, using the second one for now.
+    # (maybe we actually shouldn't do mirroring in the renderer since it might
+    # be counter-intuitive? Let's change that later.)
+    #templates = np.array([[0.3, 0.5, 1.0],
+    #                      [3.0, 5.0, 10.0]], np.float32).reshape(T, TH, TW, C)
+    templates = np.array([[1.0, 0.5, 0.3],
+                          [10.0, 5.0, 3.0]], np.float32).reshape(T, TH, TW, C)
+
+
+    # Background: HWC
+    background = np.full((H, W, C), 9.9, np.float32)
+
+    # Desired painting: HWC
+    desired = np.array([9.9,
+                        1.0*0.3,            # T1_0
+                        0.5*0.5 + 0.5*0.3,  # T1_1 + T1_0
+                        1./3.*1.0 + 1./3.*0.5 + 1./3.*3.0,  # T1_2 + T1_1 + T2_0
+                        0.5*1.0 + 0.5*5.],  # T1_2 + T2_1
+                       np.float32).reshape(H, W, C)
+
+    render = Render()
+    # Add the batch-dimension where necessary
+    img, (a, a_bg, t) = render.render1(
+        presence[np.newaxis], types[np.newaxis], templates, background[np.newaxis])
+    print("\nimg=", img, "\na=", a, "\na_bg=", a_bg, "\nt=", t)
+    np.testing.assert_allclose(img[0], desired)
+    img_loops = render.render_loops(presence[np.newaxis], types[np.newaxis], templates, background[np.newaxis])
+    print("\nimg=", img)
+    np.testing.assert_allclose(img_loops[0], desired)
